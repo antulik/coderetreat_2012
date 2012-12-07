974fa127be4406c50bd2ba6e817ca7c6ad7be512
Anton Katunin <antulik@gmail.com>
2012-12-08 09:41:03 +1000

    iter one

--- starting_points/javascript/coderetreat.js
+++ starting_points/javascript/coderetreat.js
@@ -1,3 +1,65 @@
 function foo() {
   return "qix";
 }
+
+
+function tick(world) {
+
+  var new_world = {};
+
+
+  for (var cell in world) {
+
+    var count = countAround(world, cell);
+
+    if (count == 2 || count == 3) {
+      new_world[cell] = true
+    }
+
+    var around = cellsAround(cell);
+    for (var i = 0; i < around.length; i++){
+      var _cell = around[i];
+
+      if (world[_cell] != true){
+        var _count = countAround(world, _cell);
+        if (_count == 3){
+          new_world[_cell] = true
+        }
+      }
+    }
+  }
+
+  return new_world;
+}
+
+function countAround(world, cell) {
+  var cells = cellsAround(cell);
+
+  var count = 0;
+  for (var i = 0; i < cells.length; i++){
+    if (world[cells[i]] == true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function cellsAround(cell) {
+  cell = cell.split(',')
+  var x = parseInt(cell[0]);
+  var y = parseInt(cell[1]);
+
+  var cells = [];
+  for (var _x = x - 1; _x <= x + 1; _x++) {
+
+    for (var _y = y - 1; _y <= y + 1; _y++) {
+      var key = _x + "," + _y;
+
+      if (key != cell) {
+        cells.push(key);
+      }
+    }
+  }
+
+  return cells;
+}
--- starting_points/javascript/coderetreatTest.js
+++ starting_points/javascript/coderetreatTest.js
@@ -1,4 +1,59 @@
-test('is foo "bar" ?', function() {
-    equals(foo(), "bar");
-})
+//test('is foo "bar" ?', function() {
+//    equals(foo(), "bar");
+//})
+
+test('rule 1', function(){
+  var world = {
+    '0,1':true,
+    '0,0':true
+  }
+
+  var keys = Object.keys( tick(world));
+
+  equal(keys.length, 0, 'Cell 2 neighbor cells died');
+});
+
+test('rule 2 - 3 friends', function(){
+  var world = {
+    '0,0':true,
+    '0,1':true,
+    '1,0':true,
+    '1,1':true
+  }
+
+  var new_world = tick(world);
+  var keys = Object.keys(new_world);
+
+  equal(keys.length, 4, '4 cells live');
+  deepEqual(new_world, world, "World haven't changed")
+});
+
+test('rule 2 - 2 friends', function(){
+  var world = {
+    '0,0':true,
+    '1,0':true,
+    '1,1':true
+  }
+
+  var new_world = tick(world);
+
+  ok(new_world['1,0'], "Cell with two neighbours lives");
+});
+
+test('rule 4 - Blinker (period 2)', function(){
+  var world = {
+    '1,0':true,
+    '1,1':true,
+    '1,2':true
+  }
+
+  var new_world = tick(world);
+  var keys = Object.keys(new_world);
+
+  equal(keys.length, 3, '3 cells live');
+  deepEqual(new_world, {
+    '0,1':true, '1,1':true, '2,1':true
+  }, "Blinker (period 2)")
+});
+
 
